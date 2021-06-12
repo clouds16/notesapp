f523ddedb561855a095f2446d28abc980ddb045b
Hector <hect16@gmail.com>
2021-06-12 15:08:28 -0700

    some updates

--- mongoose.js
+++ mongoose.js
@@ -1,62 +1,7 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
 });
-
-const User = mongoose.model("User", {
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	age: {
-		type: Number,
-		required: true,
-		validate(value) {
-			if (value < 0) {
-				throw new Error("Age must be a positive number");
-			}
-		},
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error("Email is invalid");
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		minLength: 8,
-		trim: true,
-		validate(value) {
-			if (value.toLowerCase().includes("password")) {
-				throw new Error('Password cannot contain word "password" ');
-			}
-		},
-	},
-});
-
-const me = new User({
-	name: "Hector",
-	age: 27,
-	email: "hect16@gmail.com",
-	password: "Thisismypw16",
-});
-
-me.save()
-	.then(() => {
-		console.log(me);
-	})
-	.catch((error) => {
-		console.log("Error:", error);
-	});
